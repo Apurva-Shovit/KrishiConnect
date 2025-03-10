02d904a20fd3e8d77558c538180fd9b82a5356a8
Apurva-Shovit <apurvshovit@gmail.com>
2025-03-10 08:53:53 +0530

    added app.js, added index.html

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+
+
+
+
+app.listen(port, () => console.log(`server running on ${port}`));
